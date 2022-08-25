a3cbc162e89adc3e5285cff5231065786053f6fe
DenisOuma <dennisouma38@gmail.com>
2022-08-25 09:08:35 +0300

    Tasklis function completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// Working on the solution for the task lis
+document.addEventListener("DOMContentLoaded", (e) => {
+	e.preventDefault();
+	// get data from the submitted form
+	const myForm = document.getElementById("create-task-form");
+	myForm.addEventListener("submit", (e) => {
+		// Prevent the default Value
+		e.preventDefault();
+		const newTask = document.getElementById("new-task-description");
+		const taskList = document.getElementById("tasks");
+		const newTaskItem = document.createElement("li");
+		newTaskItem.innerText = newTask.value;
+		taskList.appendChild(newTaskItem);
+		// Reset the dom
+		e.target.reset();
+	});
 });
